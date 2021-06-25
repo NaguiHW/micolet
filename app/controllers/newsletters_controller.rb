3501f76736327df0d6537865d7a397fe840b243f
Kalib Hackin <nagui08@hotmail.com>
2021-06-25 14:56:13 -0500

    created mailer to deliver emails

--- app/controllers/newsletters_controller.rb
+++ app/controllers/newsletters_controller.rb
@@ -4,12 +4,12 @@ class NewslettersController < ApplicationController
 
   def create
     newsletter = Newsletter.new(newsletter_params)
-    puts params[:woman_fashion]
     if params[:woman_fashion] == "0" and params[:man_fashion] == "0" and params[:kid_fashion] == "0"
       flash[:alert] = ['Debe de elegir, aunque sea, una de las preferencias.']
     else
       if newsletter.save
         flash[:notice] = '¡Usted se ha suscrito correctamente para recibir nuestras novedades!'
+        NewsletterMailer.with(user: newsletter).suscription.deliver_now
       else
         flash[:alert] = newsletter.errors.full_messages
       end
