4ffb22c64a993c8fc83e0fdfba2d2bfa2bfae132
Kalib Hackin <nagui08@hotmail.com>
2021-06-25 14:19:34 -0500

    crating controllers and flash messages when a user suscribe

--- app/controllers/newsletters_controller.rb
+++ app/controllers/newsletters_controller.rb
@@ -1,4 +1,25 @@
 class NewslettersController < ApplicationController
   def index
   end
+
+  def create
+    newsletter = Newsletter.new(newsletter_params)
+    puts params[:woman_fashion]
+    if params[:woman_fashion] == "0" and params[:man_fashion] == "0" and params[:kid_fashion] == "0"
+      flash[:alert] = ['Debe de elegir, aunque sea, una de las preferencias.']
+    else
+      if newsletter.save
+        flash[:notice] = '¡Usted se ha suscrito correctamente para recibir nuestras novedades!'
+      else
+        flash[:alert] = newsletter.errors.full_messages
+      end
+    end
+    redirect_back(fallback_location: root_path)
+  end
+
+  private
+
+  def newsletter_params
+    params.permit(:email, :woman_fashion, :man_fashion, :kid_fashion)
+  end
 end
