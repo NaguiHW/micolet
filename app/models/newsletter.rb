e07e43f2ff8bac46e7cd954703ccd5cfcfb55da3
Kalib Hackin <nagui08@hotmail.com>
2021-06-25 13:06:27 -0500

    adding newsletter model validations and finishing form view

--- app/models/newsletter.rb
+++ app/models/newsletter.rb
@@ -1,2 +1,15 @@
 class Newsletter < ApplicationRecord
+  before_save :downcase_email
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i.freeze
+  validates :email,
+            presence: true,
+            length: { maximum: 100 },
+            format: { with: VALID_EMAIL_REGEX },
+            uniqueness: { case_sensitive: false }
+
+  private
+
+  def downcase_email
+    self.email = email.downcase
+  end
 end
